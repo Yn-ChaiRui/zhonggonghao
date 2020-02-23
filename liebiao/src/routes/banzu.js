1b92ececb206b89d1df135592b19be19fb0fba84
Yn-ChaiRui <1022835@qq.com>
2020-02-23 11:05:13 +0800

    1

--- liebiao/src/routes/banzu.js
+++ liebiao/src/routes/banzu.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import { Table, Divider, Tag, Modal, Button, Tabs, Form, Input, Checkbox, Card,Menu,Layout, Breadcrumb, Icon
+import {
+  Table, Divider, Tag, Modal, Button, Tabs, Form, Input, Checkbox, Card, Menu, Layout, Breadcrumb, Icon
 } from 'antd';
 import styles from './IndexPage.css';
 import Tianjiajs from './tianjiajs'
 
 
 
-const { Column, ColumnGroup } = Table;
-const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8 },
-};
-const formTailLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8, offset: 4 },
-};
 
-
-const { TabPane } = Tabs;
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
-
-function callback(key) {
-  console.log(key);
-}
 
 class Jiaose extends React.Component {
   state = {
@@ -91,51 +75,50 @@ class Jiaose extends React.Component {
 
   render() {
 
-    
+    const { visible, confirmLoading, ModalText } = this.state;
 
     return (
 
 
-      <div style={{height:900}}>
+      <div style={{ height: 900 }}>
 
-<Card title="全部班组" extra={
+        <Card title="全部班组" extra={
 
-<div>
-<Button type="primary" onClick={this.showModal}>
-  创建班组
+          <div>
+            <Button type="primary" onClick={this.showModal}>
+              创建班组
 </Button>
-<Modal
-  title="Basic Modal"
-  visible={this.state.visible}
-  onOk={this.handleOk}
-  onCancel={this.handleCancel}
->
-  <p>Some contents...</p>
-  <p>Some contents...</p>
-  <p>Some contents...</p>
-</Modal>
-</div>
-} style={{ width: 300 }} className={styles.fuhong} >
+            <Modal
+              title="Title"
+              visible={visible}
+              onOk={this.handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={this.handleCancel}
+            >
+              <p>{ModalText}</p>
+            </Modal>
+          </div>
+        } style={{ width: 300 }} className={styles.fuhong} >
 
-<Menu>
-  <Menu.Item>初一 (GVN13)</Menu.Item>
-  <Menu.Item>语文基础班(IY9RB)</Menu.Item>
-  <Menu.Item>111(CN915)</Menu.Item>
-  <Menu.Item>双一流(XXG08)</Menu.Item>
-  <Menu.Item>985(6OW7E)</Menu.Item>
-  <Menu.Item>211(7SS0P)</Menu.Item>
-  <Menu.Item>我问问</Menu.Item>
-  <Menu.Item>456</Menu.Item>
-  <Menu.Item>Test</Menu.Item>
-  <Menu.Item>一年级</Menu.Item>
-  <Menu.Item>中班223w1</Menu.Item>
-</Menu>
-    </Card>
-    <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 850 }} className={styles.fuhong2}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
+          <Menu>
+            <Menu.Item>初一 (GVN13)</Menu.Item>
+            <Menu.Item>语文基础班(IY9RB)</Menu.Item>
+            <Menu.Item>111(CN915)</Menu.Item>
+            <Menu.Item>双一流(XXG08)</Menu.Item>
+            <Menu.Item>985(6OW7E)</Menu.Item>
+            <Menu.Item>211(7SS0P)</Menu.Item>
+            <Menu.Item>我问问</Menu.Item>
+            <Menu.Item>456</Menu.Item>
+            <Menu.Item>Test</Menu.Item>
+            <Menu.Item>一年级</Menu.Item>
+            <Menu.Item>中班223w1</Menu.Item>
+          </Menu>
+        </Card>
+        <Card size="small" title="初一 (GVN13)" extra={<a><Icon type="user-add" /><Icon type="menu" /></a>} style={{ width: 850 }} className={styles.fuhong2}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
       </div>
     );
   }
