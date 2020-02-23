3383a554cb597e067448e9703b1d24e580b9cd8d
Yn-ChaiRui <1022835@qq.com>
2020-02-23 14:10:31 +0800

    1

--- liebiao/src/routes/banzu.js
+++ liebiao/src/routes/banzu.js
@@ -6,7 +6,66 @@ import styles from './IndexPage.css';
 import Tianjiajs from './tianjiajs'
 
 
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
 
+const columns = [
+  {
+    title: '用户名',
+    dataIndex: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '姓名',
+    dataIndex: 'age',
+  },
+  {
+    title: '展示姓名',
+    dataIndex: 'address',
+  },
+  {
+    title: '操作',
+    dataIndex: 'address',
+    dataIndex: 'addressa',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: {},
+    address: <Input placeholder="Basic usage" />,
+    addressa:<Icon type="delete" />,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: {},
+    address: <Input placeholder="Basic usage" />,
+    addressa:<Icon type="delete" />,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: {},
+    address: <Input placeholder="Basic usage" />,
+    addressa:<Icon type="delete" />,
+  },
+  {
+    key: '4',
+    name: 'Jim Green',
+    age: {},
+    address: <Input placeholder="Basic usage" />,
+    addressa:<Icon type="delete" />,
+  },
+];
 
 
 class Jiaose extends React.Component {
@@ -115,9 +174,9 @@ class Jiaose extends React.Component {
           </Menu>
         </Card>
         <Card size="small" title="初一 (GVN13)" extra={<a><Icon type="user-add" /><Icon type="menu" /></a>} style={{ width: 850 }} className={styles.fuhong2}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+          <p>段国涛（DGT） 学生数2名</p>
+          <p> <Button>批量移除</Button></p>
+          <p><Table rowSelection={rowSelection} columns={columns} dataSource={data} /></p>
         </Card>
       </div>
     );
